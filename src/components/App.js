af13e3222f9089b96a98fb016dab216d5a6f2f4c
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-28 19:13:03 +0530

    Code Added

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,39 @@ import React, { useState, useEffect } from "react";
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
+    const [preview, setPreview] = useState("");
+    const [quote, setQuote] = useState("");
+    const onBtnClick = (key) => {
+        setPreview(`${preview}${key}`);
+        if (`${preview}${key}` === "forty two") {
+            fetch("https://api.quotable.io/random")
+                .then((res) => res.json())
+                .then((data) => setQuote(data.content));
+        }
+    };
 
-  return (
-    <div className="keyboard">
-      <div className="preview"></div>
-      <div>
-        {keys.map((key) => (
-          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
-            {key === " " ? "Space" : key.toUpperCase()}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="keyboard">
+            {quote === "" ? (
+                <>
+                    <div className="preview">{preview}</div>
+                    <div>
+                        {keys.map((key) => (
+                            <button
+                                key={key}
+                                id={key === " " ? `key-space` : `key-${key}`}
+                                onClick={() => onBtnClick(key)}
+                            >
+                                {key === " " ? "Space" : key.toUpperCase()}
+                            </button>
+                        ))}
+                    </div>
+                </>
+            ) : (
+                <div className="quote">{quote}</div>
+            )}
+        </div>
+    );
 };
 
 export default App;
